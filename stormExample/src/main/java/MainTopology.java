353e78263ddbc3bba0f139fad8fb7c2883aa1dec
Raghavendar Reddy Maddelavedu <raghavendarm10@gmail.com>
2020-04-19 17:54:50 -0500

    MainTopology is upadted

--- stormExample/src/main/java/MainTopology.java
+++ stormExample/src/main/java/MainTopology.java
@@ -5,4 +5,32 @@ import org.apache.storm.Config;
 import org.apache.storm.LocalCluster;
 import org.apache.storm.topology.TopologyBuilder;
 
+public class MainTopology {
+    public static void main(String[] args) throws Exception {
+        TopologyBuilder builder = new TopologyBuilder();
+        builder.setSpout("IntegerSpout",new IntegerSpout());
+        builder.setBolt("MultiplierBoult", new MultiplierBolt()).shuffleGrouping("IntegerSpout");
+
+
+        Config config   = new Config();
+        //config.setDebug(true);
+
+        LocalCluster cluster =  new LocalCluster();
+        try
+        {
+            cluster.submitTopology("HelloTopology",config,builder.createTopology());
+            Thread.sleep(10000);
+        }
+        catch (Exception e){
+            e.printStackTrace();
+
+        }
+        finally {
+            cluster.shutdown();
+        }
+
+
+    }
+}
+
 
